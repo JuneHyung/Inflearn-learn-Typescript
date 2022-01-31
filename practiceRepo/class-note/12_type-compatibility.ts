0e6fe9a3adf3592c77b5d75691d50a6805d8d4d9
JuneHyung <cjh951114@gmail.com>
2022-01-31 10:05:38 +0900

    15. 타입 호환(Type Compatibility)

---
+++ practiceRepo/class-note/12_type-compatibility.ts
@@ -0,0 +1,51 @@
+interface Developer { 
+  name: string;
+  skill: string;
+}
+
+// interface Person{
+//   name: string;
+// }
+
+class Person { 
+  name: string;
+  skill: string;
+}
+
+var developer: Developer;
+var person: Person;
+// developer = person;
+// person = developer;
+
+developer = new Person();
+
+
+// 함수
+var add = function (a: number) { 
+  console.log(a);
+}
+var sum = function (a: number, b: number) { 
+  // ...
+  // return a + b;
+}
+
+sum = add;
+// add = sum;
+
+// 제네릭
+interface Empty<T> { 
+  // ..
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> { 
+  data: T;
+}
+
+var notEmpty1: NotEmpty<string>;
+var notEmpty2: NotEmpty<number>;
+notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;
